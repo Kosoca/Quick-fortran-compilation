7712f48987bb8cf5a9a846c1e4616f2c0ea00b78
Kosoca <59687626+Kosoca@users.noreply.github.com>
2021-03-17 11:59:11 +0100

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
-
-gfortran $1 -o $2
-./$2
+contentArgument=$1;
+if [[ -z "$contentArgument" ]]; then
+  echo "Missing name file !";
+  echo " ";
+  echo "$0 -> (?)";
+else
+  echo "Starting...";
+  echo " ";
+  nameFileConverted="$(echo "$contentArgument" | sed 's/.f90//')";
+  gfortran $contentArgument -o $nameFileConverted
+  echo "Compilation...";
+  echo " ";
+  chmod +x $nameFileConverted
+  echo "Creation file at name $nameFileConverted...";
+  echo " ";
+  echo "End of script.";
+  echo "Starting of '$nameFileConverted'";
+  echo " ";
+  ./$nameFileConverted
+fi
